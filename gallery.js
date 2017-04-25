fa7607d06720466650efdc561bc138ebabad298a
ogauper <ogauper@mail.csuchico.edu>
2017-04-25 11:59:40 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
 
 
+var current = -1;
+var chico = ["Creek", "Library","Computer Lab", "Cafeteria", "Bidwell Park"];
+
 function shuffleGallery()
 {
-
+var num = Math.floor(Math.random() * (chico.length ));
+while (num == current ){
+  num = Math.floor(Math.random() * (chico.length ));
+}
+console.log(chico[num]);
+current = num;
 }
